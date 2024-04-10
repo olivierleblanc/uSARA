9d5c69485d9d681197bb0500769c0ca3b9400f50
Olivier Leblanc <oliviero3@hotmail.com>
2024-04-10 11:02:44 +0100

    readd normalization of y

--- lib/lib_utils/util_read_data_file_ROP.m
+++ lib/lib_utils/util_read_data_file_ROP.m
@@ -4,7 +4,7 @@ function [y, alpha, beta, flag_data_weighting] = util_read_data_file(dataFilenam
     
     fprintf('\nINFO: imaging weights will not be applied.');
     flag_data_weighting = false;
-    y = double(dataloaded.y(:));
+    y = double(dataloaded.y(:)) .* double(dataloaded.nW(1));
 
     alpha = double(dataloaded.alpha);
     beta = double(dataloaded.beta);
